a1ac9fde8540c528ffd2ee159e23530158571a8d
2023148050 <kyn7666@yonsei.ac.kr>
2024-11-25 20:17:40 +0900

    Deleted old project files

--- app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, DiaryEntry, ToDoItem
-
-admin.site.register(UserProfile)
-admin.site.register(DiaryEntry)
-admin.site.register(ToDoItem)
